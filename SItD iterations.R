ca466da578ab808c39f9566d473f5fa04c4c87e6
saerobson <86623301+saerobson@users.noreply.github.com>
2021-07-12 15:43:06 +0100

    Add files via upload

--- SItD iterations.R
+++ SItD iterations.R
@@ -1,12 +1,12 @@
 ##### Iterating Death, Start with alpha #####
-n=10 #n is total number of days
+n=200 #n is total number of days
 N=66100000 - 1890000#N is population size, N = S+I+D , UK-NI
 
 alpha=rep(1,n)
 alpha[1]=1
 beta=rep(0,n) #sum(beta)=1
-#Find alpha and beta first
-ro=3.203
+
+ro=0.203
 
 h=rep(0,n) #hazard function at infected age j
 
@@ -41,26 +41,39 @@ dnbinom(x=1,size=dispersion,prob=probability)
 
 P=rep(0,n)
 lamsum=rep(0,n)
+lamsum2=rep(0,n)
 p=rep(0,n)
 d=rep(0,n)
 dd=rep(0,n)
-P[1]=dnbinom(x=1,size=dispersion,prob=probability)
+c=rep(0,n)
+P[1]=dnbinom(x=1,mu=18.69,size=(1/0.0546))
 h[1]=inf_fatal_rate*dnbinom(x=1,size=dispersion,prob=probability)/(1-inf_fatal_rate*P[1])
-p[1]=dnbinom(x=1,size=dispersion,prob=probability)
-#Find h and P
+p[1]=dnbinom(x=1,mu=18.69,size=(1/0.0546))
 
-for ( i in 2:n) {p[i]=dnbinom(x=i,size=dispersion,prob=probability)
+dnbinom(size)
+dnbinom(x=3,mu=18.69,size=(1/0.0546))
+dnbinom(x=20,mu=18.69,size=0.0546)
+tstar=31.57
+
+#Find alpha, beta, h and P
+for ( i in 2:n) {p[i]=dnbinom(x=i,mu=18.69,size=(1/0.0546))
 P[i]=sum(p[1:i])
-  h[i]=inf_fatal_rate*dnbinom(x=i,size=dispersion,prob=probability)/(1-inf_fatal_rate*P[i])
+  h[i]=inf_fatal_rate*dnbinom(x=i,mu=18.69,size=(1/0.0546))/(1-inf_fatal_rate*P[i-1])
   beta[i]=pgamma(q=(i+0.5),rate=rate,shape=shape) - pgamma(q=(i-0.5),rate=rate,shape=shape)
+  if (i<floor(tstar)){ alpha[i]= 1}
+  else if(i >= ceiling(tstar)){ alpha[i]= 0.2814}
+  else {alpha[i] = tstar -floor(tstar) + 0.2814*(ceiling(tstar)-tstar)}
   }
 
 beta1=data.frame(beta)
 beta1$Day=c(1:n)
 names(beta1)=c("Probability","Day")
-barplot(beta,col = "light blue",ylim=c(0,0.25),)
 
-#Find S,I,D
+##### Plots from Figure 1 #########
+barplot(beta,col = "light blue",ylim=c(0,0.25),xlim=c(1,15),xlab="Day,j")
+barplot(p,col = "light blue",ylim=c(0,0.07),xlim=c(1,60),xlab="Day,j")
+
+#Find S,I,D, c{appendix}
 for (i in 1:n){
 
   for (j in 1:i) {
@@ -68,12 +81,29 @@ for (i in 1:n){
   if (i<n){
     I[i+1,j+1]=I[i,j]-h[j]*I[i,j]}
   }
+  lamlam=0
+  for (k in 1:i){lamlam=lamlam+lamda[i,k]*I[i,k]}
+  lamsum2[i]=lamlam
   lamsum[i]=lamda[i,1:j]%*%I[i,1:j]
   d[i]=h[1:j]%*%I[i,1:j]
   if (i<n){
     I[i+1,1]=S[i]*lamsum[i]
 
     S[i+1]=S[i]-I[i+1,1]
-    D[i+1]=D[i]+d[i]}
+    D[i+1]=D[i]+d[i]
+    c[i]=sum(I[i+1,1:j])/sum(I[i,1:j])
+    }
   
 }
+#### Number Deaths on each Day ############
+DD=data.frame(D)
+DD$Day=c(1:n)
+DD$Date=as.Date()
+DD$Death[1]=0
+for (i in 2:n){DD$Death[i]=DD$D[i]-DD$D[(i-1)]} #gives number died on each day
+
+DeathByDay=ggplot(DD)
+DeathByDay=DeathByDay + geom_point(aes(x=Day,y=Death),color="magenta",size=1)
+DeathByDay=DeathByDay+labs(title="Daily Number of Deaths")
+DeathByDay=DeathByDay+ylab("Daily Deaths")
+DeathByDay
